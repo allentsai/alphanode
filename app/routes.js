4f14d799d4cf4ebc8904e74ffcd05afca41d06c4
vagrant <vagrant@devbox>
2015-03-02 22:44:24 -0800

    Updated Routes, allowed form to save/submit

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,37 @@
 var Nerd = require('./models/nerd');
-	module.exports = function(app) {
-		app.get('/api/nerds', function(req, res){
-			Nerd.find(function(err, nerds){
-				if (err)
-					res.send(err);
-				res.json(nerds);
+var fs	 = require('fs');
+module.exports = function(app) {
+	app.get('/api/nerds', function(req, res){
+		Nerd.find(function(err, nerds){
+			if (err)
+				res.send(err);
+			res.json(nerds);
+		});
+	});
+
+	app.get('/comments.json', function (req, res){
+		console.log('eh');
+		fs.readFile('_comments.json', function (err, data){
+			res.setHeader('Content-Type', 'application/json');
+			res.send(data);
+		});
+	});
+
+	app.post( '/comments.json', function(req, res) {
+		fs.readFile('_comments.json', function(err, data){
+			var comments = JSON.parse(data);
+			comments.push(req.body);
+			fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function(error) {
+				res.setHeader('Content-Type', 'application/json');
+				res.setHeader('Cache-Control', 'no-cache');
+				res.send(JSON.stringify(comments));
 			});
-		});
-     
-		app.get('*', function(req, res) {
-			res.sendfile('./public/views/index.html');
-		});
-	};
+		});	
+	});
+
+	app.get('*', function(req, res) {
+		res.sendfile('./public/views/index.html');
+	});
+};
+
 
